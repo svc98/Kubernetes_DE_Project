5e1d18c016ec2ad25da066051842dff6d299ef50
Slomo <slomochacko248@gmail.com>
2024-05-26 16:32:22 -0400

    first commit

--- dags/fetch_and_preview.py
+++ dags/fetch_and_preview.py
@@ -7,36 +7,76 @@ from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 
 
+# def get_data(**kwargs):
+#     url = "https://raw.githubusercontent.com/svc98/AWS_Sales_Events_Batch/main/data/sales_data_sample.csv"
+#     response = requests.get(url)
+#
+#     if response.status_code == 200:
+#         df = pd.read_csv(url, header=0)
+#
+#         # Convert DF to JSON String for XCOM
+#         json_data = df.to_json(orient='records', lines=True)
+#         kwargs['ti'].xcom_push(key='data', value=json_data)
+#     else:
+#         raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
+
+# def preview_data(**kwargs):
+#     output_data = kwargs['ti'].xcom_pull(keys='data', task_ids='get_data')
+#     print(output_data)
+#     if output_data:
+#         output_data = json.loads(output_data)
+#     else:
+#         raise ValueError('No data received from XCOM')
+#
+#     # Create DF from OutputData
+#     df = pd.DataFrame(output_data)
+#     df = df.groupby('PRODUCTLINE', as_index=False).agg({'QUANTITYORDERED': 'sum', 'SALES': 'sum'})
+#
+#     # Sorting
+#     df = df.sort_values(by='SALES', ascending=False)
+#
+#     print(df[['PRODUCTLINE', 'QUANTITYORDERED', 'SALES']].head(10))
+
 def get_data(**kwargs):
-    url = "https://raw.githubusercontent.com/svc98/AWS_Sales_Events_Batch/main/data/sales_data_sample.csv"
+    import requests
+    import pandas as pd
+
+    url = 'https://raw.githubusercontent.com/airscholar/ApacheFlink-SalesAnalytics/main/output/new-output.csv'
     response = requests.get(url)
 
     if response.status_code == 200:
-        df = pd.read_csv(url, header=0)
+        df = pd.read_csv(url, header=None, names=['Category', 'Price', 'Quantity'])
 
-        # Convert DF to JSON String for XCOM
-        json_data = df.to_json(orient='records', lines=True)
+        #convert dataframe to json string from xcom
+        json_data = df.to_json(orient='records')
+
         kwargs['ti'].xcom_push(key='data', value=json_data)
     else:
         raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
 
-
 def preview_data(**kwargs):
-    output_data = kwargs['ti'].xcom_pull(keys='data', task_ids='get_data')
+    import pandas as pd
+    import json
+
+    output_data = kwargs['ti'].xcom_pull(key='data', task_ids='get_data')
     print(output_data)
     if output_data:
         output_data = json.loads(output_data)
     else:
-        raise ValueError('No data received from XCOM')
+        raise ValueError('No data received from XCom')
 
-    # Create DF from OutputData
+    # Create Dataframe from JSON data
     df = pd.DataFrame(output_data)
-    df = df.groupby('PRODUCTLINE', as_index=False).agg({'QUANTITYORDERED': 'sum', 'SALES': 'sum'})
 
-    # Sorting
-    df = df.sort_values(by='SALES', ascending=False)
+    #Compute total sales
+    df['Total'] = df['Price'] * df['Quantity']
 
-    print(df[['PRODUCTLINE', 'QUANTITYORDERED', 'SALES']].head(10))
+    df = df.groupby('Category', as_index=False).agg({'Quantity': 'sum', 'Total': 'sum'})
+
+    #sort by total sales
+    df = df.sort_values(by='Total', ascending=False)
+
+    print(df[['Category', 'Total']].head(20))
 
 
 default_args = {
