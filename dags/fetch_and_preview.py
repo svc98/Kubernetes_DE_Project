48b7ab21d4651279926bec38d48d35340e34e2ff
Slomo <slomochacko248@gmail.com>
2024-05-26 16:41:18 -0400

    first commit

--- dags/fetch_and_preview.py
+++ dags/fetch_and_preview.py
@@ -1,37 +1,6 @@
 from airflow import DAG
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow.operators.python import PythonOperator
-
-
-# def get_data(**kwargs):
-#     url = "https://raw.githubusercontent.com/svc98/AWS_Sales_Events_Batch/main/data/sales_data_sample.csv"
-#     response = requests.get(url)
-#
-#     if response.status_code == 200:
-#         df = pd.read_csv(url, header=0)
-#
-#         # Convert DF to JSON String for XCOM
-#         json_data = df.to_json(orient='records', lines=True)
-#         kwargs['ti'].xcom_push(key='data', value=json_data)
-#     else:
-#         raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
-
-# def preview_data(**kwargs):
-#     output_data = kwargs['ti'].xcom_pull(keys='data', task_ids='get_data')
-#     print(output_data)
-#     if output_data:
-#         output_data = json.loads(output_data)
-#     else:
-#         raise ValueError('No data received from XCOM')
-#
-#     # Create DF from OutputData
-#     df = pd.DataFrame(output_data)
-#     df = df.groupby('PRODUCTLINE', as_index=False).agg({'QUANTITYORDERED': 'sum', 'SALES': 'sum'})
-#
-#     # Sorting
-#     df = df.sort_values(by='SALES', ascending=False)
-#
-#     print(df[['PRODUCTLINE', 'QUANTITYORDERED', 'SALES']].head(10))
 
 def get_data(**kwargs):
     import requests
